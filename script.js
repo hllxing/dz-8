ba8cdbe967feb312cfcf4dac2d6aaf11e5ae9521
hllxing <maksimkozura13@gmail.com>
2023-04-13 00:35:03 +0300

    final

---
+++ script.js
@@ -0,0 +1,78 @@
+// let result = '';
+// for (let i = 10; i <= 20; i++){
+//     result += i + (i===20 ? '' : ',');
+// }
+// console.log(result); ---- задание 1
+
+// let result = '';
+// for (let i = 10; i <= 20; i++){
+//     result += i * i + '\n';
+// }
+// console.log(result); ---- задание 2
+
+// let result = '';
+// for (let i = 1; i <= 10; i++){
+//     result = i * 7;
+//     console.log(i + ' * ' + 7 + ' = ' + result)
+// } ---- задание 3
+
+// let result = 0;
+// for (let i = 1; i <= 15; i++) {
+//     result += i;
+// }
+// console.log(result); ---- задание 4
+
+// let result = 1;
+// for (let i = 15; i <= 35; i++){
+//     result *= i;
+// }
+// console.log(result); ---- задание 5
+
+// let result = '';
+// let sum = 0;
+// for (let i = 1; i <= 500; i++){
+//     sum += i;
+//     result = sum / 500;
+// }
+// console.log(result); ---- задание 6
+
+// let result = 0;
+// for (let i = 30; i <= 80; i++) {
+//     if (i % 2 === 0) {
+//         result += i;
+//     }
+// }
+// console.log(result); ---- задание 7
+
+// let result = '';
+// for (let i = 100; i <= 200; i++) {
+//      if (i % 3 === 0) {
+//          result += i + '\n';
+//      }
+// }
+// console.log(result); ---- задание 8
+
+// let number = prompt('Введіть натуральне число!');
+// let deviders = '';
+// let evenDeviders = '';
+// let evenDevidersSum = 0;
+// for (let i = 1; i <= number; i++){
+//     if (number % i === 0){
+//         deviders += i + '\n';
+//     }
+//     if (number % i === 0 && i % 2 ===0){
+//         evenDeviders += i + '\n';
+//         evenDevidersSum += i;
+//     }
+// }
+// console.log('Дільники числа:' + '\n' + deviders);
+// console.log('Парні дільники:' + '\n' +evenDeviders);
+// console.log(evenDevidersSum) ---- задание 9
+
+// let result = 1
+// for (let i = 1; i <= 10; i++){
+//     for (let t = 1; t <= 10; t++){
+//         result = i * t;
+//         console.log(i + ' * ' + t + ' = ' + result);
+//     }
+// } ---- задание 10
